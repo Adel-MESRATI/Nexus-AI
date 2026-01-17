3dc07960456957e5a438020fb7af0ff6861ae451
Adel <adel.mesrati@gmail.com>
2026-01-17 12:23:01 +0100

    Updated PWA icons to high quality

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = {
+  // Your existing settings are here:
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = withPWA(nextConfig);
